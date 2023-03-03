32ba4a8357ea62bc17d063fedeffadf9eadde552
deepakcit <deepakkumarcsecit@gmail.com>
2023-03-03 14:06:44 +0530

    e_commerce_games

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const gamesData = [
+  {
+    id: 1,
+    name: 'Super Mario Odyssey',
+    description: 'Explore incredible places far from the Mushroom Kingdom as you join Mario and his new ally Cappy on a massive, globe-trotting 3D adventure.',
+    price: 59.99,
+    image: 'https://cdn02.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_4/H2x1_NSwitch_SuperMarioOdyssey_image1600w.jpg',
+  },
+  {
+    id: 2,
+    name: 'The Legend of Zelda: Breath of the Wild',
+    description: 'Step into a world of discovery, exploration and adventure in The Legend of Zelda: Breath of the Wild, a boundary-breaking new game in the acclaimed series.',
+    price: 69.99,
+    image: 'https://www.giantbomb.com/a/uploads/scale_medium/0/3699/2920687-the%20legend%20of%20zelda%20-%20breath%20of%20the%20wild%20v7.jpg'
+  },
+  {
+    id: 3,
+    name: 'Animal Crossing: New Horizons',
+    description: 'Escape to a deserted island and create your own paradise as you explore, create, and customize in the Animal Crossing: New Horizons game.',
+    price: 49.99,
+    image: 'https://cdn02.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_4/H2x1_NSwitch_AnimalCrossingNewHorizons_image1600w.jpg',
+  },
+  {
+    id: 4,
+    name: 'Mario Kart 8 Deluxe',
+    description: 'Race and battle your friends in the definitive version of Mario Kart 8, only on Nintendo Switch!',
+    price: 49.99,
+    image: 'https://cdn02.nintendo-europe.com/media/images/10_share_images/games_15/nintendo_switch_4/H2x1_NSwitch_MarioKart8Deluxe_image1600w.jpg',
+  },
+  {
+    id: 5,
+    name: 'Pokémon Sword and Shield',
+    description: 'Set off on a new adventure in the Galar region with the Pokémon Sword and Pokémon Shield games!',
+    price: 59.99,
+    image: 'https://images.alphacoders.com/103/thumb-1920-1039414.png'
+  },
+];
+
+export default gamesData;
